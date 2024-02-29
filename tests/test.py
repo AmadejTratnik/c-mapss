7cb57c577fa26d3269e5f64c822d4b9305c4ea0f
AmadejTratnik <amadej.tratnik@gmail.com>
2024-02-29 10:26:05 +0100

    Added tests

--- tests/test.py
+++
@@ -1,3 +0,0 @@
-
-def testing_test():
-    assert 1 == 1
---
+++ tests/test_t.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestCase(unittest.TestCase):
+
+    def testing_test(self):
+        self.assertEqual(1,1)
+
+if __name__ == '__main__':
+    unittest.main()
