d7a65d633ad8068301bba8e6c1f482b1c7092f30
AmadejTratnik <amadej.tratnik@gmail.com>
2024-03-23 09:08:38 +0100

    Processed data access

--- app/data_access.py
+++ app/data_access.py
@@ -2,14 +2,18 @@ import os
 import pandas as pd
 
 
-def get_files_with_prefix(directory_path, prefix_list):
+def get_files_with_prefix(directory_path: str, prefix_list: list) -> list[str]:
     all_files = os.listdir(directory_path)
-    filtered_files = sorted([directory_path+file for file in all_files if any(file.startswith(prefix) for prefix in prefix_list)])       
+    filtered_files = sorted(
+        [directory_path + file for file in all_files if any(file.startswith(prefix) for prefix in prefix_list)])
     return filtered_files
 
 
+def get_dataframe(value: str) -> dict:
+    return pd.read_csv(value).to_dict('records')
+
 
 if __name__ == '__main__':
-    print(get_files_with_prefix('./data/', ['test','train']))
-    #print(make_dataframe('./data/'+'train_FD001.txt'))
+    print(get_files_with_prefix('./data/', ['test', 'train']))
+    # print(make_dataframe('./data/'+'train_FD001.txt'))
     pass
