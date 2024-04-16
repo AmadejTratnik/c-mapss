2db91224f477bf3ca12eb12d8bf903bc47e50a14
AmadejTratnik <amadej.tratnik@gmail.com>
2024-04-16 22:38:04 +0200

    Model init def, verbose bool

--- modelling/inference.py
+++ modelling/inference.py
@@ -1,28 +1,31 @@
 import numpy as np
 import sys
+
 sys.path.append('.')
 
 
 def load_jet_model(model_path):
     from keras.src.saving import load_model
-    model =load_model(model_path)
-    model.trainable =False
+    model = load_model(model_path)
+    model.trainable = False
     return model
 
-def predict_fault(model, new_x) -> int:
-    return np.argmax(model.predict(new_x), axis=-1)[0]
+
+def predict_fault(model, new_x,verbose=False) -> int:
+    return np.argmax(model.predict(new_x,verbose=int(verbose)), axis=-1)[0]
+
 
 def generate_random_input_data(batch_size, time_steps, num_features):
     input_data = np.random.rand(batch_size, time_steps, num_features)
     return input_data
+
 
 def init_models():
     model_fd001 = load_jet_model('./modelling/models/FD001_model.keras')
     model_fd002 = load_jet_model('./modelling/models/FD002_model.keras')
     model_fd003 = load_jet_model('./modelling/models/FD003_model.keras')
     model_fd004 = load_jet_model('./modelling/models/FD004_model.keras')
-    return model_fd001,model_fd002,model_fd003,model_fd004
-
+    return model_fd001, model_fd002, model_fd003, model_fd004
 
 
 if __name__ == '__main__':
